922ac5e5384dfb7652cf65628394554afb52b448
JoeDespo <jdesposito17@gmail.com>
2023-11-21 13:25:11 -0500

    Initial Commit

---
+++ CS250 Homework 5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS250 Homework 5
+//
+//  Created by Joseph D'Esposito on 11/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
